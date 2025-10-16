0ee5fb20482362353747021f72d83e3507633c4b
Naga Satya Dheeraj Anumala <dheerajanumala2688@gmail.com>
2025-10-16 18:59:59 -0400

    Initial Commit

---
+++ AllTime/AllTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllTimeApp.swift
+//  AllTime
+//
+//  Created by Naga Satya Dheeraj Anumala on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct AllTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
